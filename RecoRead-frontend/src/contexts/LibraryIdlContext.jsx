cf7e1964261da37995501254b88146fb6eed6a21
Mayankk05 <142004365+Mayankk05@users.noreply.github.com>
2025-11-03 12:22:49 +0530

    Update LibraryIdlContext.jsx

--- RecoRead-frontend/src/contexts/LibraryIdlContext.jsx
+++ RecoRead-frontend/src/contexts/LibraryIdlContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext } from 'react';
 
-// Holds a Set of googleBooksId currently known in the user's library (from current page)
 export const LibraryIdsContext = createContext(new Set());
 
 export function useLibraryIds() {
